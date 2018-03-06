83a7615e5d33c25784755ebab7f9b81dc9aeaeae
chensj2018 <csj21@sohu.com>
2018-03-06 10:00:17 +0800

    http source处理put请求成功时返回的状态码由200改为201

--- flume-ng-core/src/main/java/org/apache/flume/source/http/HTTPSource.java
+++ flume-ng-core/src/main/java/org/apache/flume/source/http/HTTPSource.java
@@ -311,6 +311,9 @@ public class HTTPSource extends AbstractSource implements
     public void doPut(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
       doPost(request, response);
+      if(response.getStatus()==HttpServletResponse.SC_OK) {
+    	  response.setStatus(HttpServletResponse.SC_CREATED);
+      }
     }
   }
 }
